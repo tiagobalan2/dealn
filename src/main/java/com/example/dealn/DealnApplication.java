e05a1806ff42ae05189b2dac7f6d93c454521804
tiagobalan <tiagobalan@edu.unifil.br>
2024-09-15 18:43:18 -0300

    inicio do projeto

---
+++ src/main/java/com/example/dealn/DealnApplication.java
@@ -0,0 +1,13 @@
+package com.example.dealn;
+// iniciando projeto
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dealn/DealnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dealn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
