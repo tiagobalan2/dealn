663925b83f791286183cc7dad94e0b5ee8b94f67
tiagobalan <tiagobalan@edu.unifil.br>
2024-09-20 00:39:07 -0300

    refactoring cors : error still

--- src/main/java/com/example/dealn/infra/cors/CorsConfig.java
+++ src/main/java/com/example/dealn/infra/cors/CorsConfig.java
@@ -6,11 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "DELETE", "PUT");
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                    .allowedHeaders("*");
+
     }
 }
